a0060b88ccddedf99a3fe4169ce795edb1b3d023
fckillernaver <fckiller@naver.com>
2025-02-19 16:26:30 +0900

    test1 init

---
+++ index.js
@@ -0,0 +1,4 @@
+const fruitButton = document.querySelector("#fruit")
+const furnitureButton = document.querySelector("#furniture")
+const phoneButton = document.querySelector("#phone")
+const todosButton = document.querySelector("#todos")
