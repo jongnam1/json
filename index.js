ffc9d519ad54888353dde0a9389d417ec8a501cc
fckillernaver <fckiller@naver.com>
2025-02-20 16:34:52 +0900

    tmdb updated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const api_url = "https://api.themoviedb.org/3/movie/popular"
 const imgUrl = "https://image.tmdb.org/t/p/w500"
 const api_key = "25db6ff26e8517fd46c2ee67944fa068"
 
@@ -10,5 +11,9 @@ const ul = document.createElement("ul")
 h1.innerText = "Loading..."
 
 let movies = []
+
+//! renderMovies 함수 만들기
+
+//! fetchMovies
 
 body.append(button, h1, ul)
